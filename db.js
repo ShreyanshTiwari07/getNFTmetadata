4212649edb7e374519a91886c8b0136ee825b1a5
Shreyansh Tiwari <122351558+ShreyanshTiwari07@users.noreply.github.com>
2024-01-03 12:25:15 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { MongoClient } = require("mongodb");
 
 async function connectToMongoDB() {
   const uri =
-    "mongodb+srv://doadmin:5Q3D1R2e49o7cp6v@fktrnode2023mongodb-07e504fb.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=fktrnode2023mongodb";
+    "";
 
   try {
     const client = new MongoClient(uri);
